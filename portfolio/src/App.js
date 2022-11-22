42d1fc808e2955a062a550ca05753a0d3e4cec6d
chewzzz <chewziqing@gmail.com>
2022-11-22 22:06:58 +0800

    Add skills section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,12 +4,14 @@ import Nav from './components/Nav';
 import Info from './components/Info';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div>
       <Nav />
       <Info />
+      <Skills />
       <Projects />
       <Footer />
     </div>
