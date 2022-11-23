8e23d7d0814d59ec91ebfcec371479aa4f113fc9
chewzzz <chewziqing@gmail.com>
2022-11-23 22:56:48 +0800

    Seperate data from react components

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -5,51 +5,87 @@ const projectData = [
         desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus voluptatem minima ducimus explicabo, vero, sunt recusandae aspernatur fuga ratione, rerum quibusdam dicta est. Reprehenderit omnis delectus esse dicta cupiditate error.',
         skills: [
             {
-                name: '',
-                imgLink: ''
+                name: 'Node js',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg'
+            },
+            {
+                name: 'Express',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg'
+            },
+            {
+                name: 'Mongodb',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg'
+            },
+            {
+                name: 'HTML',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+            },
+            {
+                name: 'CSS',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
             }
         ],
         srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/Exercise-Tracker',
         pageLink: 'https://boilerplate-project-exercisetracker.chewzi1.repl.co/'
     },
     {
-        name: 'Exercise Tracker',
+        name: 'Timestamp Microservice',
         imgLink: '',
         desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus voluptatem minima ducimus explicabo, vero, sunt recusandae aspernatur fuga ratione, rerum quibusdam dicta est. Reprehenderit omnis delectus esse dicta cupiditate error.',
         skills: [
             {
-                name: '',
-                imgLink: ''
+                name: 'Node js',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg'
+            },
+            {
+                name: 'Express',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg'
+            },
+            {
+                name: 'HTML',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+            },
+            {
+                name: 'CSS',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
             }
         ],
-        srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/Exercise-Tracker',
-        pageLink: 'https://boilerplate-project-exercisetracker.chewzi1.repl.co/'
+        srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/Timestamp-Microservice',
+        pageLink: 'https://boilerplate-project-timestamp.chewzi1.repl.co'
     },
     {
-        name: 'Exercise Tracker',
+        name: 'Rolling Dices',
         imgLink: '',
         desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus voluptatem minima ducimus explicabo, vero, sunt recusandae aspernatur fuga ratione, rerum quibusdam dicta est. Reprehenderit omnis delectus esse dicta cupiditate error.',
         skills: [
             {
-                name: '',
-                imgLink: ''
+                name: 'React',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+            },
+            {
+                name: 'CSS',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
             }
         ],
-        srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/Exercise-Tracker',
-        pageLink: 'https://boilerplate-project-exercisetracker.chewzi1.repl.co/'
+        srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/tenzies-game',
+        pageLink: 'https://Tenzies-Game.chewzi1.repl.co'
     },
     {
-        name: 'Exercise Tracker',
+        name: 'Meme Generator',
         imgLink: '',
         desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus voluptatem minima ducimus explicabo, vero, sunt recusandae aspernatur fuga ratione, rerum quibusdam dicta est. Reprehenderit omnis delectus esse dicta cupiditate error.',
         skills: [
             {
-                name: '',
-                imgLink: ''
+                name: 'React',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+            },
+            {
+                name: 'CSS',
+                imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
             }
         ],
-        srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/Exercise-Tracker',
-        pageLink: 'https://boilerplate-project-exercisetracker.chewzi1.repl.co/'
+        srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/meme-generator',
+        pageLink: 'https://meme-generator.chewzi1.repl.co/'
     },
 ]
 export default projectData
