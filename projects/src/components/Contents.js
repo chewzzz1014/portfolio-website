31558254be11c71c3b1f79d624e855f7363f1ffc
chewzzz <chewziqing@gmail.com>
2022-11-24 13:00:57 +0800

    Add mui drawer

---
+++ projects/src/components/Contents.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contents(props) {
+    return (
+        <div className='contents'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus adipisci debitis dolore, odit, fugit nobis perferendis voluptas in quam, commodi illum molestiae cupiditate mollitia ullam. Labore delectus earum nam dignissimos!</p>
+        </div>
+    )
+}
---
+++ projects/src/components/ScrollBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ScrollBar(props) {
+    return (
+        <div className='scrollbar'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit, eius sunt exercitationem quibusdam repellat laboriosam perspiciatis quia rem temporibus aliquam quam nihil pariatur quas id, nulla iure fugit possimus.</p>
+        </div>
+    )
+}
