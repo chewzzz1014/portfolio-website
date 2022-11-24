3fccbf20cef9d9278bba3cb927dfbfa375eb0327
chewzzz <chewziqing@gmail.com>
2022-11-24 14:57:06 +0800

    Add cards in accordian

--- projects/src/components/Contents.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Contents(props) {
-    return (
-        <div className='contents'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus adipisci debitis dolore, odit, fugit nobis perferendis voluptas in quam, commodi illum molestiae cupiditate mollitia ullam. Labore delectus earum nam dignissimos!</p>
-        </div>
-    )
-}
