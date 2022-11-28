e4428d38ee427601b3741f7a90c8bc356cbfa7c8
chewzzz <chewziqing@gmail.com>
2022-11-28 21:23:51 +0800

    Rename footer's id to contacts

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -3,7 +3,7 @@ import { AiFillLinkedin, AiFillGithub, AiFillMail } from 'react-icons/ai'
 
 export default function Footer() {
     return (
-        <footer id='footer'>
+        <footer id='contacts'>
             <div className='footer-icons'>
                 <a href="mailto:chewziqing@gmail.com" target='__blank'><AiFillMail size='2.5em' color='white' /></a>
                 <a href="https://www.linkedin.com/in/ziqingchew" target="_blank" ><AiFillLinkedin size='2.5em' color='white' /></a>
