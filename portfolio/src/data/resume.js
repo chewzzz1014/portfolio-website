91a58742c858992f5d5a911464d18aa82daa38c4
chewzzz <chewziqing@gmail.com>
2023-01-23 22:08:34 +0800

    Update resume

--- portfolio/src/data/resume.js
+++ portfolio/src/data/resume.js
@@ -1,5 +1,5 @@
 const resume = {
-    url: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website/portfolio/src/data/resume/Zi_Qing_Chew_Resume_v2.pdf',
+    url: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website/portfolio/src/data/resume/Zi_Qing_Chew_Resume_v3.pdf',
     fileName: 'Zi_Qing_Chew_Resume.pdf'
 }
 
