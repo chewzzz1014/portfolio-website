6d518e8f34f0671ad15018ef23673661dee9acfa
chewzzz <chewziqing@gmail.com>
2023-01-29 09:58:25 +0800

    Display resume based on the latest commit id

--- portfolio/src/data/resume.js
+++ portfolio/src/data/resume.js
@@ -1,7 +1,11 @@
 // allow for always displaying the latest version of resume (same file name with diff commit id)
 
+const commits = [
+    '683f65045ea3c81811621e509ae5c705bdc421a4'
+]
+
 const resume = {
-    url: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website/portfolio/src/data/resume/Zi_Qing_Chew_Resume_v3.pdf',
+    url: `https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website@${commits[commits.length - 1]}/portfolio/src/data/resume/Zi_Qing_Chew_Resume.pdf`,
     fileName: 'Zi_Qing_Chew_Resume.pdf'
 }
 
