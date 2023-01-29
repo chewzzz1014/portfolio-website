e6625e045bdb34f86cf900677546111ef666f7dc
chewzzz <chewziqing@gmail.com>
2023-01-29 09:53:55 +0800

    Add comments

--- portfolio/src/data/resume.js
+++ portfolio/src/data/resume.js
@@ -1,3 +1,5 @@
+// allow for always displaying the latest version of resume (same file name with diff commit id)
+
 const resume = {
     url: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website/portfolio/src/data/resume/Zi_Qing_Chew_Resume_v3.pdf',
     fileName: 'Zi_Qing_Chew_Resume.pdf'
