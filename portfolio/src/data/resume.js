4a632ed18dd5f76a77668c7fd270c624bd9d1fb0
chewzzz <chewziqing@gmail.com>
2023-02-10 16:03:13 +0800

    Update resume latest url

--- portfolio/src/data/resume.js
+++ portfolio/src/data/resume.js
@@ -4,7 +4,8 @@ const commits = [
     'caa0011e5d0ac5887c7ca3565f0ade9e799a9c4f',
     '957148a7335fa1fbc39653c61fefbfb6d6cfd674',
     '0ff3d72ceeba1f356e5c88871a784b3eb8b7965f',
-    '4cd5da33487f03da5d6dfa56c04fd59ac83bd5c6'
+    '4cd5da33487f03da5d6dfa56c04fd59ac83bd5c6',
+    '8cb92c6b1a0967d51126e9781058fc25bea00ef2'
 ]
 
 const resume = {
